973383a7799f75c29849e197e58628c014ad4ae7
aldi140 <aldi.agung853@gmail.com>
2026-04-15 11:07:56 +0700

    update login from url

--- fe/src/hooks/useAuth.js
+++ fe/src/hooks/useAuth.js
@@ -29,7 +29,7 @@ export const useAuth = () => {
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate("/login");
+    navigate("https://koplink.net/login.php");
   };
 
   const handleAuthFromUrl = () => {
