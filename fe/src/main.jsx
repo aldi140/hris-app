a48b55464a92941f3d89c58084af9da46139e8c6
aldi140 <aldi.agung853@gmail.com>
2026-04-15 11:01:49 +0700

    update login from url

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -7,21 +7,6 @@ import './index.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-const params = new URLSearchParams(window.location.search);
-const urlToken = params.get("token");
-const urlUser = params.get("user");
-
-if (urlToken && urlUser) {
-  try {
-    const user = JSON.parse(decodeURIComponent(urlUser));
-    localStorage.setItem("token", urlToken);
-    localStorage.setItem("user", JSON.stringify(user));
-    window.history.replaceState({}, "", window.location.pathname);
-  } catch (err) {
-    console.error("Gagal parse auth dari URL:", err);
-  }
-}
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
